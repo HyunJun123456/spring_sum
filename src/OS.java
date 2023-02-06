84545d9cb5f3bfd8fcb12758a261ad9c64c14b0e
HyunJun123456 <juni303@chauniv.ac.kr>
2023-02-06 21:54:51 +0900

    운영체제가 뭐길래?

---
+++ src/OS.java
@@ -0,0 +1,28 @@
+
+public class OS { // Operating System
+	public static void main(String[] args) {
+		/*
+		 * operating system
+		 * 스마트폰을 컴퓨터라고도 부름
+		 * 계산기는 calculator라고 부름
+		 * 컴퓨터의 정의는 information : 정보를 처리하는 기계
+		 * binary digit -> bit (정보의 최소 단위)
+		 * 불확실성을 측정해서 양으로 만드는 것
+		 * 정보의 상태 변환을 위한 물리적인 장치 (0에서 1로, 1에서 0으로)
+		 * 범용성
+		 * - NOT, AND, OR 게이트만으로 모든 계산을 할 수 있다.
+		 * 컴퓨터가 모든 것을 다 계산 할 수 있는 것은 아님
+		 * 컴퓨터의 할아버지 : Alan Turing
+		 * 내장형 프로그램 도입 : 폰 노이만
+		 * 메모리에 프로그램을 저장하는 방식
+		 * fetch -execute 방식
+		 * 프로그램 : H/W에 task를 수행할 수 있는 명령어들의 집합
+		 * 우리가 코드를 짜면 compile 해서 기계어로 만들어줌
+		 * 운영체제도 프로그램인가요?
+		 * 컴퓨터에서 항상 실행중인 프로그램
+		 * H/W에 직접 컨택하기 힘드니 O/S가 담당함
+		 * 프로세스, 자원, user interface를 관리함
+		 * 
+		 * */
+	}
+}
